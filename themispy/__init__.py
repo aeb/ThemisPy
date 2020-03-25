78fd38883d8979c6432ddc4753295a9c10cddb9c
abroderick <abroderick@perimeterinstitute.ca>
2020-03-25 17:26:01 -0400

    Added versioneer

---
+++ themispy/__init__.py
@@ -0,0 +1,37 @@
+"""
+This is a set of postprocessing tools for Themis analyses.
+
+Tools may be accessed via
+
+>>> import themispy as ty
+
+Available subpackages
+---------------------
+diag
+   Chain, sampler, and general management diagnostics
+data
+   Residual, systematics, calibration
+modvis
+   Model image visulalization
+chain
+   Tools for navigating and modifying chains
+"""
+
+__version__="0.1"
+__author__="Themis Development Team"
+__bibtex__ = r"""@Article{Themis:2020,
+  %%% Fill in from ADS!
+}"""
+
+__all__=['chain'] #'diag', 'chain', 'data', 'modvis']
+from . import *
+
+
+
+       
+
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
