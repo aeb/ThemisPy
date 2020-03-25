220436e22315260f9fcdaa5d3153584db5752820
abroderick <abroderick@perimeterinstitute.ca>
2020-03-25 17:32:29 -0400

    First setup file commits

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import versioneer
+
+setup(name='ThemisPy',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Postprocessing tools for Themis',
+      author='Themis Development Team',
+      author_email='Themis@perimeterinstitute.ca',
+      url='https://github.com/aeb/ThemisPy',
+      packages=find_packages(),
+      install_requires=['numpy','matplotlib']
+     )
+
+
+
+
+
