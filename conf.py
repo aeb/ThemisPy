75d509cc81aab11c5bf22437a86debb5bdac3b08
abroderick <abroderick@perimeterinstitute.ca>
2020-03-25 19:28:04 -0400

    Adjusting sphinx configuration, still need to figure out autodoc

---
+++ conf.py
@@ -0,0 +1,72 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./themispy/'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'ThemisPy'
+copyright = '2020, Themis Development Team'
+author = 'Themis Development Team'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['docs/_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+import versioneer
+version = versioneer.get_version()
+release = version
+show_authors = True
+
+master_doc = 'docs/frontmatter/index'
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme_default = 'bizstyle'
+html_theme_path_default = []
+
+
+try:
+    import rtcat_sphinx_theme
+    html_theme = "rtcat_sphinx_theme"
+    html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
+except ModuleNotFoundError :
+    html_theme = html_theme_default
+    html_theme_path = []
+    print("WARNING: Could not import rtcat_sphix_theme.  Will used default.")
+
+
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['docs/_static']
