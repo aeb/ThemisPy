92c77116e91a89bec030f129a4f30e80859e71d0
abroderick <abroderick@perimeterinstitute.ca>
2020-03-26 03:11:54 -0400

    Removed whitespace

--- conf.py
+++ conf.py
@@ -84,7 +84,6 @@ except ModuleNotFoundError :
     print("WARNING: Could not import rtcat_sphix_theme.  Will used default.")
 
 
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
