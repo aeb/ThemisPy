c786e893df928003d54a5f5d9bae3c70193300e4
abroderick <abroderick@perimeterinstitute.ca>
2020-04-02 17:47:55 -0400

    rebuilt docs

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.28.gacd89e2.dirty',
+    VERSION: '0+untagged.38.g67d58f4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
