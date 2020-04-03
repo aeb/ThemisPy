12001a2026ac576c01267cf5290be9bb08b4cb21
abroderick <abroderick@perimeterinstitute.ca>
2020-04-02 20:22:12 -0400

    rebuilt docs

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.46.g0875d63',
+    VERSION: '0+untagged.52.g6f590ee.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
