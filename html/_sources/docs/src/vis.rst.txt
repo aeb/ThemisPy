347c3ec129fbb3d9bbcfcf04e469ff53ba67d7ad
abroderick <abroderick@perimeterinstitute.ca>
2020-04-07 13:56:44 -0400

    rebuilt docs

---
+++ html/_sources/docs/src/vis.rst.txt
@@ -0,0 +1,18 @@
+.. module:: vis
+
+.. _vis:
+
+Vis
+==============================
+
+Calibration
+------------------------------
+.. automodule:: vis.calibration
+   :members:
+
+Plotting Functions
+------------------------------
+.. automodule:: vis.typlot
+   :members:
+
+
--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.97.g30acb18',
+    VERSION: '0+untagged.104.g852d5bd.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
