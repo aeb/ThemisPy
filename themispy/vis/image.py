Fixed normalization issue for themages

themispy/vis/image.py
@@ -695,7 +695,7 @@ class model_image_splined_raster(model_image) :
           (numpy.ndarray) Array of intensity values at positions (x,y) in :math:`Jy/\\mu as^2`.
         """
 
-        f = np.transpose(np.exp(np.array(self.parameters).reshape([self.Nx,self.Ny])))
+        f = np.transpose(np.exp(np.array(self.parameters).reshape([self.Nx,self.Ny]))) * uas2rad**2
 
         xtmp = np.linspace(-0.5*self.fovx,0.5*self.fovx,self.Nx)
         ytmp = np.linspace(-0.5*self.fovy,0.5*self.fovy,self.Ny)
@@ -801,7 +801,7 @@ class model_image_adaptive_splined_raster(model_image) :
           (numpy.ndarray) Array of intensity values at positions (x,y) in :math:`Jy/\\mu as^2`.
         """
 
-        f = np.transpose(np.exp(np.array(self.parameters[:-3]).reshape([self.Nx,self.Ny])))
+        f = np.transpose(np.exp(np.array(self.parameters[:-3]).reshape([self.Nx,self.Ny]))) * uas2rad**2
         #f = np.fliplr(f)
         f = np.flipud(f)
         

themispy/vis/polimage.py
@@ -374,7 +374,7 @@ class model_polarized_image_adaptive_splined_raster(model_polarized_image) :
         S = []
 
         Ndim = self.Nx*self.Ny
-        fI = np.flipud(np.transpose(np.exp(np.array(self.parameters[:Ndim]).reshape([self.Nx,self.Ny]))))
+        fI = np.flipud(np.transpose(np.exp(np.array(self.parameters[:Ndim]).reshape([self.Nx,self.Ny])))) * uas2rad**2
         p = np.flipud(np.transpose(np.exp(np.array(self.parameters[Ndim:2*Ndim]).reshape([self.Nx,self.Ny]))))
         evpa = np.flipud(np.transpose(np.array(self.parameters[2*Ndim:3*Ndim]).reshape([self.Nx,self.Ny])))
         muV = np.flipud(np.transpose(np.array(self.parameters[3*Ndim:4*Ndim]).reshape([self.Nx,self.Ny])))